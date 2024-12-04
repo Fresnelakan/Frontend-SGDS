93c589c1f97cd9891243859cb7be5f3a616e2297
Fresnel AKAN <fresnelakan@gmail.com>
2024-12-04 14:17:41 +0100

    Version1

---
+++ src/main/java/com/sgds/demo/FrontEndSgdsApplication.java
@@ -0,0 +1,13 @@
+package com.sgds.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontEndSgdsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrontEndSgdsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sgds/demo/FrontEndSgdsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sgds.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontEndSgdsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
